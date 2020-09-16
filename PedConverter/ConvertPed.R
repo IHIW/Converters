d507657a3ad2a57ebec731645decda47cbdd62ca
Ben Matern <ben.matern@gmail.com>
2020-09-16 17:29:25 +0200

    feature/101

--- PedConverter/ConvertPed.R
+++ PedConverter/ConvertPed.R
@@ -49,16 +49,80 @@ convertPedFile <- function(workingDirectory, bucketName, pedFilename) {
   return('Success')
 }
 
-
-lambdaHandler <- function(jsonInput) {
-  print("This is the Json Input:")
-  print(jsonInput)
+lambdaHandler <- function(Records)
+{
+  inputClass = class(Records)
+  
+  #library(jsonlite)
+  #jsonText = paste("{Records:",Records,"}")
+  
+  #returnValue=class(Records)
+  #s3Object=Records["s3"]
+  
+  #fromJsons3Object = fromJSON(s3Object)
+  
+  #s3ObjectNames = names(s3Object)
+  #objectObject=s3Object["object"]
+  
+  
+  #returnValue=s3Object
+  #returnValue=objectObject
+  #firstRecord=Records[1]
+  
+  #parsedJson = fromJSON(Records)
+  
+  #returnValue = class(s3Object)
+  returnValue = inputClass
+  
+  #returnValue=firstRecord
+  #returnValue = jsonText
+  #print("This is the Json Input:")
+  #print(jsonInput)
   #install.packages('kinship2')
-  print('Inside the lambda handler')
-  convertPedFile("/home/bmatern/UMCU/Test Files/PED", "ihiw-management-upload-staging", "1497_1600100387164_PED_kazu.ped.tab")
-  return('Successful lambda handler.')
+  #print('Inside the lambda handler')
+  #convertPedFile("/home/bmatern/UMCU/Test Files/PED", "ihiw-management-upload-staging", "1497_1600100387164_PED_kazu.ped.tab")
+  #return(paste("Success:",returnValue))
+  return(returnValue)
   
 } 
 
-#lambdaHandler()
+RecordsInput <- '[
+    {
+      "eventVersion": "2.1",
+      "eventSource": "aws:s3",
+      "awsRegion": "eu-central-1",
+      "eventTime": "2020-09-15T12:28:06.851Z",
+      "eventName": "ObjectCreated:Put",
+      "userIdentity": {
+        "principalId": "AWS:AIDAU7VWKTLKR2Y6EIZLL"
+      },
+      "requestParameters": {
+        "sourceIPAddress": "31.201.47.123"
+      },
+      "responseElements": {
+        "x-amz-request-id": "6WERAV5V1TCXCR0W",
+        "x-amz-id-2": "qlJTWjA8YjxzqIfr0vlLzspvT1sOXGLxwC7qCaoS444MkJfthrg1ON3InX44K5isCaPD+vguhUlBO16qbnTmzFHEAlM0b9G3"
+      },
+      "s3": {
+        "s3SchemaVersion": "1.0",
+        "configurationId": "SNSonPUT",
+        "bucket": {
+          "name": "ihiw-management-upload-staging",
+          "ownerIdentity": {
+            "principalId": "A341HYTWE9KERG"
+          },
+          "arn": "arn:aws:s3:::ihiw-management-upload-staging"
+        },
+        "object": {
+          "key": "1497_1600100387164_PED_kazu.ped.tab.pdf",
+          "size": 5971,
+          "eTag": "5b508276f6e38773c54041c9c3e93400",
+          "versionId": "_Vf9RPhv2ae4Piq_fy0Z.6UORotF426w",
+          "sequencer": "005F60B35A5C93FCC0"
+        }
+      }
+    }
+  ]'
+
+#lambdaHandler(Records=RecordsInput)
 
