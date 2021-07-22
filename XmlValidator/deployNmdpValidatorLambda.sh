b89a8b7f837c487a1e1c70e1a15954edd8678f7f
Ben Matern <ben.matern@gmail.com>
2021-07-22 15:54:59 +0200

    updating hml validators to use step functions

--- XmlValidator/deployNmdpValidatorLambda.sh
+++ XmlValidator/deployNmdpValidatorLambda.sh
@@ -1,12 +1,10 @@
 # This script will install dependencies, bundle the lambda function, and deploy to the AWS lambda environment.
 # Y must have the AWS commandline environment already installed
 # And you must have previously run "aws configure" to set up your machine for access to the AWS Environment.
-# You need to provide the config file validation_config.yml with entries url: {} username: {} password: {}
 # Feel free to try it without activating the virtual environment (remove the line source $ENVIRONMENT_PATH"/bin/activate")
 # it may or may not be necessary depending on your local python environment.
 PROJECT_PATH="/home/bmatern/github/Converters/XmlValidator"
 ENVIRONMENT_PATH="/home/bmatern/github/Converters/venv"
-HANDLER_FILE="NmdpPortalValidation.py"
 LAMBDA_FUNCTION="validateXmlNMDPStaging"
 #LAMBDA_FUNCTION="validateXmlNMDPProd"
 
@@ -18,7 +16,6 @@ rm function.zip
 # Install package(s)
 source $ENVIRONMENT_PATH"/bin/activate"
 pip install --target ./package lxml
-pip install --target ./package pyyaml
 pip install --target ./package requests
 deactivate
 
@@ -28,12 +25,7 @@ zip -r9 $PROJECT_PATH"/function.zip" .
 
 # Zip Script
 cd ..
-zip -g function.zip $HANDLER_FILE
-zip -j -g function.zip ../Common/IhiwRestAccess.py
-
-
-# Zip Config File
-zip -g function.zip validation_config.yml
+zip -g function.zip NmdpPortalValidation.py
 
 # Upload to AWS
 aws lambda update-function-code --function-name $LAMBDA_FUNCTION --zip-file fileb://function.zip
